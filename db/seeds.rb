e121cbe193de084544a469adf2b8755b268ea46c
RachelNolan <mail@rachelnolan.com>
2015-02-09 16:18:54 +0000

    Seeding database now uses find_or_create to prevent duplicate values being created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 CSV.foreach("product_data.csv") do |col|
-  Product.create(
+  Product.find_or_create_by(
     :product_code => col[0],
     :product_name => col[1],
     :price => col[2]
--- spec/features/adding_products_to_cart_feature_spec.rb
+++ spec/features/adding_products_to_cart_feature_spec.rb
@@ -1,12 +1,12 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# context 'Each product on the homepage' do
+context 'Each product on the homepage' do
 
-#   it "should have an 'add to cart' button" do
-#     Product.create(product_code: 1, product_name: 'Lavender Heart', price: 9.95)
-#     visit '/'
-#     expect(page).to have_content 'Lavender Heart'
-#   end
+  it "should have an 'add to cart' button" do
+    Product.create(product_code: 1, product_name: 'Lavender Heart', price: 9.95)
+    visit '/'
+    expect(page).to have_selector("input[type=submit][value='Press Me']")
+  end
 
-# end
+end
 
