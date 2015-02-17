9816d538687ee916cb4b0356d45e9abf777260dd
RachelNolan <mail@rachelnolan.com>
2015-02-17 13:26:40 +0000

    Line Item unit price is now method in model rather than pulling direct from product.price

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,9 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
 
+  def unit_price
+    product.price
+  end
+
+
 end
--- spec/features/view_cart_feature_spec.rb
+++ spec/features/view_cart_feature_spec.rb
@@ -3,21 +3,33 @@ require 'rails_helper'
 feature "When viewing the cart" do
 
   context "each product" do 
-    it "should have an 'remove from cart' button" do
+
+    before do 
       Product.create(product_code: 1, product_name: 'Lavender Heart', price: 9.95)
       visit '/'
       click_button 'Add to Cart'
+    end 
+
+    it "should have an 'remove from cart' button" do
       expect(page).to have_selector(:link_or_button, 'Remove from Cart')
     end
 
     it "should only list once with the quantity of said item" do 
-      Product.create(product_code: 1, product_name: 'Lavender Heart', price: 9.95)
-      visit '/'
-      click_button 'Add to Cart'
       visit '/'
       click_button 'Add to Cart'
       expect(page).to have_content('Quantity: 2')
     end
+
+    it "should display an item unit price" do 
+      expect(page).to have_content('Unit Price: £9.95')
+    end 
+
+    # it "should display a total item price" do 
+    #   visit '/'
+    #   click_button 'Add to Cart'
+    #   expect(page).to have_content('Total Item Price: £19.90')
+    # end 
+
   end
 
   context "it should display" do 
