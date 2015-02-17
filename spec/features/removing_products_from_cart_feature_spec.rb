e9a1f99778e8bf4ca6d80e7c09f174365eafd2d7
RachelNolan <mail@rachelnolan.com>
2015-02-17 11:53:35 +0000

    Fixed test for emptying a cart

--- spec/features/removing_products_from_cart_feature_spec.rb
+++ spec/features/removing_products_from_cart_feature_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 context "When removing an item from the cart" do
-  
-  it "the cart line items should update " do
+
+  before do 
     Product.create(product_code: 1, product_name: 'Lavender Heart', price: 9.95)
     visit '/'
     click_button 'Add to Cart'
+  end
+
+  it "the cart line items should update " do
     click_button 'Remove from Cart'
-    expect(current_path).not_to have_content 'Lavender Heart'
+    expect(page).not_to have_content 'Lavender Heart'
   end
 end
